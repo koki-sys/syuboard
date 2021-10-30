b997a9072cbef53efd7c0a1c61a6a067b5888673
koki-sys <soraisu7@gmail.com>
2021-10-30 13:28:44 +0900

    ファイルの統合を行いました。

---
+++ project/Home.php
@@ -0,0 +1,13 @@
+<?php require '../header.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-white" >
+    <a class="d-block mx-auto" href="../../index.php"  style="text-align: center">
+        <img src="../images/syuboard.svg" alt="logo">
+    </a>
+</nav>
+<div class="container">
+    <div class="row-center">
+        <div>
+        <h2>掲示板</h2> <input type="text">
+        </div>
+    </div>
+</div>
---
+++ project/newlogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body style="background-color:#73E2A7;">
+<div class="box">
+<form action="newlogincomp.php" method="post">
+  <label class="ms-5 mb-1 mt-3">ニックネーム</label><br>
+  <input class="form-control mx-auto" type="text" name="nickname" style="width:75%"><br>
+  <label class="ms-5 mb-1">Email</label><br>
+  <input class="form-control mx-auto" type="mail" size="40" name="mail" style="width:75%"><br>
+  <label class="ms-5 mb-1">パスワード</label><br>
+  <input class="form-control mx-auto" type="password" size="40" name="password" style="width:75%"><br>
+  
+  <input type="submit" value="送信する">
+</form>
+</div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+</body>
+</html>
