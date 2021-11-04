bfbbfc2544624e584bfcc1b9f81d5f228a30dd32
koki-sys <soraisu7@gmail.com>
2021-11-04 14:05:36 +0900

    ホーム画面のヘッダーを作成しました。

---
+++ project/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/Home.css" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <title>syuboard</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <ul class="navbar-nav mr-auto">
+            <a class="logo d-block" href="./index.php">
+                <img src="./images/header/syuboard.png" alt="logo" width="200" height="auto">
+            </a>
+        </ul>
+        <ul class="navbar-nav list-group list-group-horizontal">
+            <li class="nav-item mr-2 ml-2">
+                <a href="login.php" class="text-center" style="color: #DEF4C6; font-size: xx-small">
+                    <img src="./images/header/login.png" class="d-block mx-auto" width="25" height="auto" />
+                    <p style="color: #DEF4C6; font-size: xx-small" class="text-center m-0">ログイン</p>
+                </a>
+            </li>
+            <li class="nav-item mr-2 ml-2">
+                <a href="shinki.php">
+                    <img src="./images/header/register.png" class="d-block mx-auto" width="25" height="auto" />
+                    <p style="color: #B1CF5F; font-size: xx-small" class="text-center m-0">新規登録</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
