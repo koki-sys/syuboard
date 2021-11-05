e0dc86fed79d012787c5ffd1c33c41a1ea1b64c0
Lulu191 <93236849+Lulu191@users.noreply.github.com>
2021-11-05 12:05:28 +0900

    変更

--- project/index.php
+++ project/index.php
@@ -1,16 +1,29 @@
-<?php require './header.php'; ?>
-<div class="container-fluid">
-    <div class="row-center">
-        <from>
-            <div class="form-row">
-                <div class="form-group col">
-                    <h2>掲示板</h2>
-                </div>
-                <div class="form-group col">
-                    <input type="text" placeholder="ジャンル名で検索">
-                </div>
-            </div>
-        </from>
+</html><!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>SYUBOARDホーム画面</title>
+<link rel="stylesheet" href="../css/Home.css" type="text/css">
+</head>
+<?php require '../header.php'; ?>
+<!--nav 設定　-->
+<body>
+      <from>
+        <div class="form-row text-center">
+          <div class="form-group col">
+            <h2>掲示板</h2>
+          </div>
+          <div class="form-group col">
+            <input type="text" style="position: absolute; right: 60px;" size="30" placeholder="ジャンル名で検索">
+          </div>
+        </div>
+      </from>  
+</div>
+<div calss="container-fluid">
+    <div class="row">
+      <div class="sured text-center">
+        <a href="php"><img src="../images/sured.png" width="800" height="150"></a>
+      </div>
     </div>
 </div>
 
