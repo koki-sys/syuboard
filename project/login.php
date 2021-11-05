0fba4df10fa900625db65127f825a8e30e4fb494
take65 <91449889+take65@users.noreply.github.com>
2021-11-05 10:44:26 +0900

    Update login.php

--- project/login.php
+++ project/login.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title></title>
+<link rel="stylesheet" href="./css/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
-<body style="background-color:#73E2A7;">
-<div class="box">
-<form action="newlogincomp.php" method="post">
-  <label class="ms-5 mb-1">Email</label><br>
-  <input class="form-control mx-auto" type="mail" size="40" name="mail" style="width:75%"><br>
-  <label class="ms-5 mb-1">パスワード</label><br>
-  <input class="form-control mx-auto" type="password" size="40" name="password" style="width:75%"><br>
-  
-  <input type="submit" value="送信する">
-</form>
+<body>
+<div class="container">
+  <div class="center">
+  <div class="box mart">
+    <div class="box2">
+      <form action="logincheck.php" method="post">
+        <div class="left">
+          <label class="mt-3 ms-5 mb-1 left">Email</label><br>
+        </div>
+        <input class="form-control mx-auto" type="mail" size="40" name="email" style="width:75%"><br>
+        <div class="left">
+          <label class="ms-5 mb-1 left">パスワード</label><br>
+        </div>
+        <input class="form-control mx-auto" type="password" size="40" name="password" style="width:75%"><br>
+    </div>
+      <div class="btns">
+      <input type="button" value="　戻る　" onclick="location.href='Home.php'" class="btn btn-outline-secondary mb-3">
+      <input type="submit" value="ログイン" class="btn2 btn mb-3">
+      </div>
+    </form>
+    </div>
+</div>
 </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
 </body>
 </html>
