28cd77652633d050abf39428bedde4d922d5d4a7
Lulu191 <93236849+Lulu191@users.noreply.github.com>
2021-11-11 12:41:33 +0900

    変更、追加

--- project/index.php
+++ project/index.php
@@ -22,28 +22,50 @@
 <!--スレッド　設定　-->
 <div calss="container-fluid">
   <div class="row">
+  <div class="col-mx-8 .offset-md-2">
     <div class="keijiban">
       <div class="sured1">
         <a href="php"><img src="./images/index/sured.png" width="800" height="120"></a>
+        <div class="henshu1">
+          <img src="./images/index/･･･.png">
+        </div>
+        <div class="prof1">
+          <img src="./images/index/prof img.png" width="30" height="30">
+        </div>
       </div>
       <div class="tagu1">
         <img src="./images/index/taguitiran.png" width="800" height="40">
+        <h4>タグの一覧表示</h4>
       </div>
       <div class="sured2">
         <a href="php"><img src="./images/index/sured.png" width="800" height="120"></a>
+        <div class="henshu2">
+          <img src="./images/index/･･･.png">
+        </div>
+        <div class="prof2">
+          <img src="./images/index/prof img.png" width="30" height="30">
+        </div>
       </div>
       <div class="tagu2">
         <img src="./images/index/taguitiran.png" width="800" height="40">
+        <h4>タグの一覧表示</h4>
       </div>
       <div class="sured3">
         <a href="php"><img src="./images/index/sured.png" width="800" height="120"></a>
+        <div class="henshu3">
+          <img src="./images/index/･･･.png">
+        </div>
+        <div class="prof3" >
+          <img src="./images/index/prof img.png" width="30" height="30">
+        </div>
       </div>
       <div class="tagu3">
         <img src="./images/index/taguitiran.png" width="800" height="40">
+        <h4>タグの一覧表示</h4>
       </div>
     </div>
   </div>
-
+  </div>
 </div>
 
 </body>
