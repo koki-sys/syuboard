6b44332be46ef4584116d66ad1b501c5717c6301
koki-sys <soraisu7@gmail.com>
2021-11-13 00:34:46 +0900

    チャット機能の骨組みの作成しました。

--- project/app/telephone/footer.php
+++ project/app/telephone/footer.php
@@ -1,5 +1,10 @@
-<script src="//cdn.webrtc.ecl.ntt.com/skyway-4.4.2.js"></script>
 <script src="./time.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="jquery.panelslider.min.js"></script>
+<script>
+    $('#chat').panelslider();
+</script>
 </body>
+
 </html>
--- project/app/telephone/header.php
+++ project/app/telephone/header.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+    <script src="//cdn.webrtc.ecl.ntt.com/skyway-4.4.2.js"></script>
     <link rel="stylesheet" href="../../css/header.css" />
     <link rel="stylesheet" href="../../css/body.css" />
+    <link rel="stylesheet" href="../../css/sidebar.css" />
     <script src="./dropmenu.js"></script>
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <title>syuboard</title>
@@ -23,7 +25,9 @@
                 <span class="mb-0 h6 light-green-text align-middle" id="call-time-up"></span>
             </li>
             <li class="nav-item mr-2 ml-3">
-                <img src="../../images/header/chat.svg" />
+                <a href="#right-panel" id="chat">
+                    <img src="../../images/header/chat.svg" />
+                </a>
             </li>
             <li class="nav-item mr-2 ml-3 dropdown">
                 <span class="m-0 light-green-text align-middle dropdown__btn" id="dropdown__btn">・・・</span>
