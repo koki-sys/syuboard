4e7efa90bd2ca82634ea029d64636ef3396b7798
Lulu191 <93236849+Lulu191@users.noreply.github.com>
2021-11-22 12:44:07 +0900

    変更

--- project/profile.php
+++ project/profile.php
@@ -8,28 +8,34 @@
 <?php require './header2.php'; ?>
 <body>
   <div class="container">
-    <h2 class="mx-l">プロフィール</h2>
+    <h2>プロフィール</h2>
     <div class="raw">
-    <div class="col-md-1"></div>
-      <div class="col-md-5">
-        <div class="profile">
-          <img src="./images/index/prof img.png" width="130" height="130">
-          <input type="text" style="position: absolute; top: 190; left: 380; width: 100%; height: 55px;" placeholder="ニックネーム">
+      <div class="col-md-1"></div>
+        <div class="col-md-5">
+          <div class="profile">
+            <img src="./images/index/prof img.png" width="100" height="100">
+            <input type="text" id="name" style="position: absolute; top: 135; left: 160; width: 80%; height: 55px;" placeholder="ニックネーム">
+          </div>
         </div>
-      </div>
-      <div class="col-md-6"></div>
+        <div class="col-md-6"></div>
     </div>
     <div class="raw">
-      <div class="comment">
-        <div class="col-md-2"></div>
-        <div class="col-md-7">
-          <h3>コメント</h3>
-          <input type="text" class="form-control" style="position: absolute; top: 400; left: 290;  height: 200px;">
-          <input type="button" placeholder="確定">
-        </div>
-        <div class="col-md-3"></div>
-      </div>
-</div>  
+      <form>
+          <div class="comment">
+            <div class="col-md-2"></div>
+              <div class="col-md-9">
+                <h4>コメント</h4>
+                <input type="text" class="form-control" style="position: absolute; top: 320; left: 100; width: 100%; height: 230px;">
+              </div>
+            <div class="col-md-1"></div>
+              <div class="col-md-1"></div>
+              <div class="form-group col-md-10">
+                <input type="submit" class="btn btn-primary" value="確定" style="position: absolute; top: 580; left: 99%; width: 140px; height: 50px">
+              </div>
+              <div class="col-md-1"></div>
+          </div>
+      </form>
+    </div> 
     
   </div>
 </body>
