56c1bc2cb5fc9b2ad07b22ad8ee4c63a4da8df82
Lulu191 <93236849+Lulu191@users.noreply.github.com>
2021-11-25 13:05:31 +0900

    変更・追加

---
+++ project/frend.php
@@ -0,0 +1,29 @@
+</html><!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>フレンド表示画面</title>
+<link rel="stylesheet" href="./css/profile.css" type="text/css">
+</head>
+<?php require './header2.php'; ?>
+<body>
+    <h2>フレンド</h2>
+<div class="container-fluid">
+  <div calss="raw">
+    <div class="col-md-2">
+        <img src="./images/index/frend.png" width="70" height="70">
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-2">
+    </div>
+  </div>    
+</div>
+</body>
+</html>
