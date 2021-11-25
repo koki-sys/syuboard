55d9e702548401bb92d1368eaacdb994733c830f
Lulu191 <93236849+Lulu191@users.noreply.github.com>
2021-11-25 11:28:32 +0900

    移行

--- project/profile.php
+++ project/profile.php
@@ -3,12 +3,39 @@
 <head>
 <meta charset="utf-8">
 <title>SYUBOARDプロフィール画面</title>
-<link rel="stylesheet" href="../css/profile.css" type="text/css">
+<link rel="stylesheet" href="./css/profile.css" type="text/css">
 </head>
 <?php require './header2.php'; ?>
 <body>
-  <div class="container-fluid">
-    <h3 class="mx-l">プロフィール</h3>
+  <div class="container">
+    <h2>プロフィール</h2>
+    <div class="raw">
+      <div class="col-md-1"></div>
+        <div class="col-md-5">
+          <div class="profile">
+            <img src="./images/index/prof img.png" width="100" height="100">
+            <input type="text" id="name" style="position: absolute; top: 135; left: 160; width: 80%; height: 55px;" placeholder="ニックネーム">
+          </div>
+        </div>
+        <div class="col-md-6"></div>
+    </div>
+    <div class="raw">
+      <form>
+          <div class="comment">
+            <div class="col-md-2"></div>
+              <div class="col-md-9">
+                <h4>コメント</h4>
+                <input type="text" class="form-control" style="position: absolute; top: 320; left: 100; width: 100%; height: 230px;">
+              </div>
+            <div class="col-md-1"></div>
+              <div class="col-md-1"></div>
+              <div class="form-group col-md-10">
+                <input type="submit" class="btn btn-primary" value="確定" style="position: absolute; top: 580; left: 99%; width: 140px; height: 50px">
+              </div>
+              <div class="col-md-1"></div>
+          </div>
+      </form>
+    </div> 
     
   </div>
 </body>
