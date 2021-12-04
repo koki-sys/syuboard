51667b6d9d89cc2d8cf50d450bb8a3263a5e5277
koki-sys <soraisu7@gmail.com>
2021-12-04 11:41:26 +0900

    レイアウトの修正を行いました。

--- project/index.php
+++ project/index.php
@@ -1,76 +1,46 @@
-</html><!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>SYUBOARDホーム画面</title>
-<link rel="stylesheet" href="./css/index.css" type="text/css">
-</head>
 <?php require './header.php'; ?>
-<body>
-  <div class="container-fluid">
-      <from>
-        <div class="form-row text-center">
-          <div class="form-group col-md-6">
-            <h2>掲示板</h2>
-          </div>
-          <div class="form-group col-md-6">
-            <input type="text" style="position: absolute; top: 30; right: 75px; width: 40%; height: 30px;" placeholder="ジャンル名で検索">
-          </div>
-        </div>
-      </from> 
-  </div> 
-<!--スレッド　設定　-->
-<script src="../js/shiritori_game.js"></script>
-<div calss="container-fluid">
-  <div class="row">
-    <div class="col-md-2"></div>
-    <div class="col-md-8">
-      <div class="keijiban">
-        <div class="sured1">
-          <a href="php"><img src="./images/index/sured.png" width="80%" height="120"></a>
-          <div class="henshu1">
-            <img src="./images/index/･･･.png" width="20" height="4">
-          </div>
-          <div class="prof1">
-            <img src="./images/index/prof img.png" width="30" height="30">
-          </div>
-        <div class="tagu1">
-          <img src="./images/index/taguitiran.png" width="80%" height="40" >
-          <h4>タグの一覧表示</h4>
-        </div>
-        <div class="sured2">
-          <a href="php"><img src="./images/index/sured.png" width="80%" height="120"></a>
-          <div class="henshu2">
-            <img src="./images/index/･･･.png" width="20" height="4">
-          </div>
-          <div class="prof2">
-            <img src="./images/index/prof img.png" width="30" height="30">
-          </div>
-        </div>
-        <div class="tagu2">
-          <img src="./images/index/taguitiran.png" width="80%" height="40">
-          <h4>タグの一覧表示</h4>
-        </div>
-        <div class="sured3">
-          <a href="php"><img src="./images/index/sured.png" width="80%" height="120"></a>
-          <div class="henshu3">
-            <img src="./images/index/･･･.png" width="20" height="4">
-          </div>
-          <div class="prof3" >
-            <img src="./images/index/prof img.png" width="30" height="30">
-          </div>
-        </div>
-        <div class="tagu3">
-          <img src="./images/index/taguitiran.png" width="80%" height="40">
-          <h4>タグの一覧表示</h4>
-        </div>
-      </div>
-    </div>
-    </div>
-    <div class="col-md-2"></div>
-  </div>
-</div>
+<?php
+require './app/controller/BoardController.php';
 
+use Controller\BoardController;
+
+$boards = new BoardController();
+?>
+<body>
+    <div class="container-fluid">
+        <div class="row mt-4 ml-4">
+            <div class="col-md-1"></div>
+            <div class="col-md-7">
+                <h2 class="page-title">掲示板</h2>
+            </div>
+            <div class="col-md-3">
+                <form action="#" method="post">
+                    <input type="text" class="form-control" placeholder="ジャンル名で検索" />
+                </form>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-6 mt-3">
+                <?php foreach ($boards->index() as $board) : ?>
+                    <div class="board card shadow-sm mt-3">
+                        <div class="card-body">
+                            <a href="#" class="float-left">
+                                <!-- AWSのS3から取得 -->
+                                <img src="./images/index/prof img.png" width="30" height="30">
+                                <?= $board["title"]; ?>
+                            </a>
+                            <small class="float-right">・・・</small><br />
+                            <span class="mt-4 float-right">ニックネーム</span>
+                        </div>
+                        <div class="card-footer tag" style="background-color: #389B72">タグの一覧表示</div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
 </body>
 
 </html>
