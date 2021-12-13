3a5544c1a8d71c842f0a3905d1f9a4875896e84b
2101043 <tatsuya.1221.12@ezweb.ne.jp>
2021-12-13 12:15:41 +0900

    ログイン処理　エラーのテスト削除
    エラー後のパスワードを確認するためのコードが入ってたので置き換えました。

--- project/logincheck.php
+++ project/logincheck.php
@@ -1,54 +1,57 @@
-<?php
++<?php
 require './database/connect.php';
-
+require 'loginkey.php';
 use Connection;
-
 session_start();
 unset($_SESSION['error']);
-$pass = $_POST['password'];
-// $hash = password_hash($pass, PASSWORD_DEFAULT);
-$pdo = Connection\conn();
+        $pass = $_POST['password'];
+        $pdo = Connection\conn();
+        $c_t = openssl_encrypt($pass, 'AES-128-ECB', $key);
+        $nickname = $_POST["nickname"];
 
-$nickname = $_POST["nickname"];
 
-try {
-    $sql = $pdo->prepare("select *,count(*) as cnt from customer where name = ?");
-    $sql->execute([$nickname]);
 
-    $j = true;
-    foreach ($sql as $row) {
-        //ログイン失敗時
-        if ($row['cnt'] == 0) {
-            $j = false;
-            $_SESSION['error'] = 'error1';
-            $_SESSION['q'] = '1';
-            header('Location: login.php');
-        } else {
-            //ログイン成功時
-            $db_pass = $row['password'];
-            if (password_verify($hash, $db_pass)) {
-                $_SESSION['user']['name'] = $row['name'];
-                header('Location: Home.php');
-                exit;
-            } else {
+    try {
+        $sql = $pdo -> prepare("select *,count(*) as cnt from customer where name = ?");
+        $sql-> execute([$nickname]);
+
+        $j = true;
+        foreach($sql as $row){
+            //ログイン失敗時
+            if($row['cnt'] == 0){
+                $j = false;
                 $_SESSION['error'] = 'error1';
-                $_SESSION['q'] = $db_pass;
-                $_SESSION['b'] = $hash;
+                $_SESSION['q'] = '1';
                 header('Location: login.php');
+            }else{
+                //ログイン成功時
+                //$db_pass  = openssl_decrypt($row['password'], 'AES-128-ECB', $key);
+                $db_pass  = $row['password'];
+                if($c_t == $db_pass){
+                    $_SESSION['user']['name'] = $row['name'];
+                    $_SESSION['user']['email'] = $row['email'];
+                    $_SESSION['user']['password'] = $row['password'];
+                    header('Location: index.php');
+                    exit;
+                }else{
+                $_SESSION['error'] = 'error1';
+                
+                header('Location: login.php');
+                }
+
             }
         }
-    }
-} catch (PDOException $e) {
-    $_SESSION['error'] = 'error2';
-    header('Location: login.php');
-}
+    }catch (PDOException $e) {
+        $_SESSION['error'] = 'error2';
+        header('Location: login.php');
+    }			
 ?>
-<?php
-if ($j) :
-else :
+<?php 
+    if($j):
+    else:
 ?>
-    <script>
-        location.href = "login.php";
-        exit;
-    </script>
+<script>
+    location.href = "login.php";
+    exit;
+</script>
 <?php endif; ?>
