40517c8b86bb1d2a1ba0ebb0a0f588aef1310793
koki-sys <soraisu7@gmail.com>
2022-01-10 16:20:27 +0900

    ログアウト画面のhtmlを修正しました。

--- project/logout.php
+++ project/logout.php
@@ -7,12 +7,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
     <meta http-equiv="refresh" content="3;URL=./index.php">
-    </head>
     <style>
         html,body {
             background-color: #FCFCFC;
         }
     </style>
+    </head>
 <body >
     <?php
     session_start();
